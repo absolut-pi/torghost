e247915c6ed9c2032f90b3d8cfcf170318712e1d
illegal-instruction-co <machinetherapist2@gmail.com>
2023-09-20 11:33:12 +0300

    add service files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SERVICE_NAME="torghost.service"
+
+sudo systemctl disable "$SERVICE_NAME"
+sudo systemctl stop "$SERVICE_NAME"
+
+sudo cp -f "./$SERVICE_NAME-service.py" "/usr/local/bin/$SERVICE_NAME.py"
+sudo cp -f "./$SERVICE_NAME" "/lib/systemd/system/$SERVICE_NAME"
+
+sudo systemctl daemon-reload
+sudo systemctl enable "$SERVICE_NAME"
+sudo systemctl start "$SERVICE_NAME"
